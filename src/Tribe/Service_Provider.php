be669a66b17d2a72049be21a0ad2a7d5b77fd89e
Matthew Batchelder <borkweb@gmail.com>
2020-03-17 21:05:28 -0400

    Fixing comments

--- src/Tribe/Service_Provider.php
+++ src/Tribe/Service_Provider.php
@@ -37,9 +37,9 @@ class Service_Provider extends \tad_DI52_ServiceProvider {
 	}
 
 	/**
-	 * Registers the provider handling all the 1st level filters and actions for Views v2.
+	 * Registers the provider handling assets
 	 *
-	 * @since 4.7.5
+	 * @since 1.0.0
 	 */
 	protected function register_assets() {
 		$assets = new Assets( $this->container );
@@ -49,9 +49,9 @@ class Service_Provider extends \tad_DI52_ServiceProvider {
 	}
 
 	/**
-	 * Registers the provider handling all the 1st level filters and actions for Views v2.
+	 * Registers the provider handling all the 1st level filters and actions for the extension
 	 *
-	 * @since 4.7.5
+	 * @since 1.0.0
 	 */
 	protected function register_hooks() {
 		$hooks = new Hooks( $this->container );
@@ -60,58 +60,5 @@ class Service_Provider extends \tad_DI52_ServiceProvider {
 		// Allow Hooks to be removed, by having the them registered to the container.
 		$this->container->singleton( Hooks::class, $hooks );
 		$this->container->singleton( 'events-happening-now.views.hooks', $hooks );
-	}
-
-	/**
-	 * Sets up the geolocation classes allowing their filtering.
-	 *
-	 * @since 4.7.9
-	 */
-	protected function register_geolocation_classes() {
-		$this->container->bind( Geo_Loc_API_Service::class, function () {
-			/**
-			 * Filters the Geo Location API Service object that should be used to resolve addresses to a set of
-			 * coordinates.
-			 *
-			 * If not provided, then the Google Maps API service  will be used.
-			 *
-			 * @since 4.7.9
-			 *
-			 * @param Geo_Loc_Handler null|$geo_location_handler A Geo Location handler object, it must implement
-			 *                        the `Tribe\Events\Pro\Views\V2\Geo_Loc\Services\Service_Interface` interface.
-			 *
-			 * @see   Tribe\Events\Pro\Views\V2\Geo_Loc\Services\Google_Maps for the default service.
-			 */
-			$geo_location_service = apply_filters( 'tribe_events_pro_views_v2_geo_location_service', null );
-
-			if ( null !== $geo_location_service ) {
-				return $geo_location_service;
-			}
-
-			return $this->container->make( Google_Maps::class );
-		} );
-
-		$this->container->singleton( Geo_Loc_Handler::class, function () {
-			/**
-			 * Filters the Geo Location Handler object that should be used to resolve geo location search requests
-			 * and fence queries.
-			 *
-			 * If not provided, then the default Geocoding Handler will be used.
-			 *
-			 * @since 4.7.9
-			 *
-			 * @param Geo_Loc_Handler null|$geo_location_handler A Geo Location handler object, it must implement
-			 *                        the `Tribe\Events\Pro\Views\V2\Geo_Loc\Handler_Interface` interface.
-			 *
-			 * @see   Tribe\Events\Pro\Views\V2\Geo_Loc\Geocoding_Handler for the default resolver.
-			 */
-			$geo_location_handler = apply_filters( 'tribe_events_pro_views_v2_geo_location_handler', null );
-
-			if ( null !== $geo_location_handler ) {
-				return $geo_location_handler;
-			}
-
-			return $this->container->make( Geocoding_Handler::class );
-		} );
 	}
 }
